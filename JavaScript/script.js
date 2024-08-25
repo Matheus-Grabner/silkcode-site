9fe54b29b9ebe80d5b88b3b60c1c3b40b88be22e
Matheus Grabner <101662483+Matheus-Grabner@users.noreply.github.com>
2024-08-25 12:52:18 -0300

    !

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -8,34 +8,40 @@ let btn = document.getElementById("openModal");
 let span = document.getElementsByClassName("close")[0];
 
 // Quando o usuário clicar no botão, abre o modal 
-btn.onclick = function() {
+btn.onclick = function () {
   modal.style.display = "block";
 }
 
 // Quando o usuário clicar no <span> (x), fecha o modal
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
 }
 
 // Quando o usuário clicar em qualquer lugar fora do modal, fecha o modal
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
 }
 
+// Fecha o modal quando o usuário pressionar a tecla Esc
+document.addEventListener('keydown', function (event) {
+  if (event.key === "Escape") {
+    modal.style.display = "none";
+  }
+});
+
 // Capturar o formulário
 let form = document.getElementById("tabela");
 
 // Função para lidar com o envio do formulário
-form.onsubmit = function(event) {
+form.onsubmit = function (event) {
   event.preventDefault(); // Impede o envio do formulário para o servidor
 
   // Pega os valores dos campos
   let nome = document.getElementById("nome").value;
   let email = document.getElementById("email").value;
   let telefone = document.getElementById("telefone").value;
-  console.log(telefone)
 
   // Exemplo de como armazenar os dados localmente
   localStorage.setItem("nome", nome);
@@ -44,9 +50,4 @@ form.onsubmit = function(event) {
 
   // Fechar o modal após enviar
   modal.style.display = "none";
-
-  // Exemplo: exibir os dados armazenados no console (apenas para verificação)
-  console.log("Nome:", nome);
-  console.log("Email:", email);
-  console.log("Telefone:", telefone);
 };
